a44d5dad51d58b97db10a9886c8035597eb4c03e
warruda01 <warruda01@gmail.com>
2025-03-01 20:22:03 -0300

    exercicios

---
+++ exercicios.py
@@ -0,0 +1,17 @@
+#Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+numero_1 = int (input("Digite o primeiro número --> "))
+numero_2 = int (input("Digite o segundo número --> "))
+soma = (numero_1) + (numero_2)
+print(soma)
+
+#Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+resto_divisao = (numero_1) % 5
+print(resto_divisao)
+
+#Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+multiplica = (numero_1) * (numero_2)
+print(multiplica)
+
+#Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+divide = (numero_1) // (numero_2)
+print(divide)
